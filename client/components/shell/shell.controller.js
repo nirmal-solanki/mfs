6c46315973992cf2a73c5221cec539902c492dae
nirmal-solanki <nirmal.todev@gmail.com>
2016-08-25 23:34:51 +0530

    remove things and main page ui changes.

--- client/components/shell/shell.controller.js
+++ client/components/shell/shell.controller.js
@@ -1,61 +1,29 @@
 'use strict';
 
 angular.module('medsmindApp')
-  .controller('ShellCtrl', function ($mdSidenav, $mdDialog, $scope, $location, Auth) {
+    .controller('ShellCtrl', function ($mdSidenav, $mdDialog, $scope, $location, Auth) {
 
-    
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.isAdmin = Auth.isAdmin;
+        $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
+        $scope.logout = function () {
+            Auth.logout();
+            $location.path('/login');
+        };
 
-    $scope.toggleLeft = function() {
-      $mdSidenav('left').toggle();
-    };
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        };
 
-    var originatorEv;
-    $scope.openMenu = function($mdOpenMenu, ev) {
-      originatorEv = ev;
-      $mdOpenMenu(ev);
-    };
+        $scope.toggleLeft = function () {
+            $mdSidenav('left').toggle();
+        };
 
-    $scope.notificationsEnabled = true;
-    $scope.toggleNotifications = function() {
-      $scope.notificationsEnabled = !$scope.notificationsEnabled;
-    };
-
-    $scope.redial = function() {
-      $mdDialog.show(
-        $mdDialog.alert()
-          .targetEvent(originatorEv)
-          .clickOutsideToClose(true)
-          .parent('body')
-          .title('Suddenly, a redial')
-          .content('You just called a friend; who told you the most amazing story. Have a cookie!')
-          .ok('That was easy')
-        );
-      originatorEv = null;
-    };
-
-    $scope.checkVoicemail = function() {
-      // This never happens.
-    };
-
-    $scope.showAddDialog = function($event) {
-      var parentEl = angular.element(document.body);
-      $mdDialog.show({
-        parent: parentEl,
-        targetEvent: $event,
-        templateUrl: 'components/shell/dialog/dialog.html',
-        controller: 'DialogController'
-      });
-    };
-  });
+        var originatorEv;
+        $scope.openMenu = function ($mdOpenMenu, ev) {
+            originatorEv = ev;
+            $mdOpenMenu(ev);
+        };
+    });
--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,6 @@ module.exports = function (app) {
 
     // Insert routes below
     app.use('/api/medicines', require('./api/medicine'));
-    app.use('/api/things', require('./api/thing'));
     app.use('/api/users', require('./api/user'));
     app.use('/api/auth', require('./auth'));
 
