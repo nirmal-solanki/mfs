52063eae8810a8fc70faa9b3fa149815fda67236
nirmal-solanki <nirmal.todev@gmail.com>
2016-08-25 23:55:23 +0530

    add angular-toastr and settings ui changes

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -1,21 +1,19 @@
 'use strict';
 
 angular.module('medsmindApp')
-  .controller('SettingsCtrl', function ($scope, User, Auth) {
-    $scope.errors = {};
+    .controller('SettingsCtrl', function ($scope, User, Auth, toastr) {
+        $scope.errors = {};
 
-    $scope.changePassword = function(form) {
-      $scope.submitted = true;
-      if(form.$valid) {
-        Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
-        .then( function() {
-          $scope.message = 'Password successfully changed.';
-        })
-        .catch( function() {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
-          $scope.message = '';
-        });
-      }
-		};
-  });
+        $scope.changePassword = function (form) {
+            $scope.submitted = true;
+            if (form.$valid) {
+                Auth.changePassword($scope.user.oldPassword, $scope.user.newPassword)
+                    .then(function () {
+                        toastr.success('Password successfully changed.');
+                    })
+                    .catch(function () {
+                        toastr.error('Incorrect password');
+                    });
+            }
+        };
+    });
--- client/app/app.js
+++ client/app/app.js
@@ -10,7 +10,8 @@ angular
         'btford.socket-io',
         'ui.router',
         'ngMaterial',
-        'ngMdIcons'
+        'ngMdIcons',
+        'toastr'
     ])
 
     .config(function ($mdThemingProvider) {
@@ -44,6 +45,33 @@ angular
 
         $locationProvider.html5Mode(true);
         $httpProvider.interceptors.push('authInterceptor');
+    })
+    .config(function(toastrConfig) {
+        angular.extend(toastrConfig, {
+            allowHtml: false,
+            closeButton: true,
+            closeHtml: '<button>&times;</button>',
+            extendedTimeOut: 1000,
+            iconClasses: {
+                error: 'toast-error',
+                info: 'toast-info',
+                success: 'toast-success',
+                warning: 'toast-warning'
+            },
+            messageClass: 'toast-message',
+            onHidden: null,
+            onShown: null,
+            onTap: null,
+            progressBar: true,
+            tapToDismiss: true,
+            templates: {
+                toast: 'directives/toast/toast.html',
+                progressbar: 'directives/progressbar/progressbar.html'
+            },
+            timeOut: 5000,
+            titleClass: 'toast-title',
+            toastClass: 'toast'
+        });
     })
 
     .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
