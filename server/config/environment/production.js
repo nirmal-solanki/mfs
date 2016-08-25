fecbe3d3947c4fbc2d54e55b8946294a62c2c4ca
nirmal-solanki <nirmal.todev@gmail.com>
2016-08-25 21:07:38 +0530

    modify grunt task

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://mfs:123456@ds023664.mlab.com:23664/mfs'
+            'mongodb://localhost/medsmind-dev'
   }
 };
